e12b5ee7377e81a558256b3bef9e49f809ee938c
thekimpeople <thekimpeople@gmail.com>
2025-05-28 07:53:27 +0300

    first commit

--- components/general/LoginPage.tsx
+++ components/general/LoginPage.tsx
@@ -1,7 +1,9 @@
 "use client"
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import React, { useState } from 'react'
 import ReCAPTCHA from "react-google-recaptcha";
+import LanguageSwitcher from '../LanguageSwitcher';
 
 
 export default function LoginPage() {
@@ -23,11 +25,18 @@ export default function LoginPage() {
       };
 
   return (
-    <div className='grid md:grid-cols-2 h-screen'>
-        <div className='bg-gray-100 flex items-center justify-center'></div>
-        <div className='flex items-center justify-center'>
-            <div className='w-full max-w-md p-8 rounded-lg'>
-                <h2 className='text-2xl font-bold mb-6'>{t('Login')}</h2>
+    <div className='grid md:grid-cols-2 h-screen bg-white'>
+        <div className='bg-gray-100 flex items-center justify-center bg-[url(/images/jpg/oldladybanker.jpg)] bg-cover bg-center'></div>
+        <div className='bg-white'>
+            <div className='flex flex-row items-center justify-end p-4 absolute top-0 right-0'>
+            <LanguageSwitcher/>
+            </div>
+            
+            <div className='flex items-center justify-center h-screen'>
+            <div className='w-full max-w-md p-8 rounded-lg '>
+                <Image src="/images/jpg/caritas_logo.png" alt="Logo" width={180} height={100} className='mb-4 flex items-center justify-center' />
+                <p className='text-2xl font-bold mb-6'>{t('login')}</p>
+                <p className='text-lg mb-4'>{t('welcome')}</p>
                 <form onSubmit={handleLogin} className='space-y-4'>
                     <div className='mb-4'>
                         <input type='text' id='userId' placeholder='Enter your user id' className='w-full p-2 border border-gray-300 rounded-md' required />
@@ -35,7 +44,7 @@ export default function LoginPage() {
                     <div className='mb-4'>
                         <input type='password' placeholder='Enter your password' id='password' className='w-full p-2 border border-gray-300 rounded-md' required />
                     </div>
-                    <button type='submit' className='w-full bg-primary text-white py-2 rounded hover:bg-secondary'>Login</button>
+                    <button type='submit' className='w-full bg-primary text-white py-2 rounded hover:bg-secondary'>{t('loginbutton')}</button>
                     
                         <ReCAPTCHA
                             sitekey="6LdfW0wrAAAAAKALu4qKDrzajNZCZ3S7tHTbnXLe"
@@ -44,6 +53,7 @@ export default function LoginPage() {
                         />
                     
                 </form>
+            </div>
             </div>
             
             </div>
